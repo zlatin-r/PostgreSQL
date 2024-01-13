9fcba6eef6fe8cd9424d56d6be50503a37137103
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-13 14:57:43 +0200

    Solution to the task

--- Data Aggregation - Exercise/10.wizard_view.sql
+++ Data Aggregation - Exercise/10.wizard_view.sql
@@ -1,22 +1,18 @@
 CREATE VIEW
-    view_wizard_deposits_with_expiration_date_before_1983_08_17
-AS SELECT
-    CONCAT_WS(' ', first_name, last_name) AS "Wizard Name",
-    deposit_start_date AS "Start Date",
-    deposit_expiration_date AS "Expiration Date",
-    deposit_amount AS "Amount"
+	view_wizard_deposits_with_expiration_date_before_1983_08_17
+AS SELECT DISTINCT
+ 	first_name || ' ' || last_name AS "Wizard Name",
+ 	deposit_start_date AS "Start Date",
+ 	deposit_expiration_date AS "Expiration Date",
+ 	deposit_amount AS "Amount"
 FROM
-    wizard_deposits
+ 	wizard_deposits
 WHERE
-    deposit_expiration_date <= '1983-08-17'
+ 	deposit_expiration_date <= '1983-08-17'
 GROUP BY
     "Wizard Name",
-    deposit_start_date,
-    deposit_expiration_date,
-    deposit_amount
+    "Start Date",
+    "Expiration Date",
+    "Amount"
 ORDER BY
-    deposit_expiration_date;
-
-
-
-
+ 	"Expiration Date" ASC;
