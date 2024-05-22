e52b4228ed6fef06f7efd5e1c1cf9827e9e0bc91
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-05-22 14:34:47 +0300

    Solution to the task

--- Built-in Functions - Exercise/01.river_info.sql
+++ Built-in Functions - Exercise/01.river_info.sql
@@ -1,7 +1,10 @@
 CREATE VIEW
     view_river_info
 AS
-SELECT CONCAT_WS(' ', 'The river', "river_name", 'flows into the', outflow, 'and is', length, 'kilometers long.')
+SELECT CONCAT_WS(' ', 'The river', "river_name",
+                 'flows into the', outflow,
+                 'and is', length,
+                 'kilometers long.')
            AS
            "River Information"
 FROM
