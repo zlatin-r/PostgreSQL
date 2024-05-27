0d60928794c23b31e1d99470e31d030786e46eab
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-05-27 16:50:40 +0300

    Solution to the task

--- Data Aggregation - Exercise/19.2.foreign_key.sql
+++ Data Aggregation - Exercise/19.2.foreign_key.sql
@@ -9,10 +9,12 @@ CREATE TABLE
 );
 
 /*
+
  CREATE TABLE
     employees_projects(
         id SERIAL PRIMARY KEY,
         employee_id INT REFERENCES employees(id),
         project_id INT REFERENCES projects(id)
 );
+
  */
