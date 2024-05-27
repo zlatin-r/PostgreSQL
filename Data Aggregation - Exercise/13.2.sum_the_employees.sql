6ee31c36469d151d6c3b965ec6f1bc9acbe7062c
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-05-27 15:41:00 +0300

    Solution to the task

--- Data Aggregation - Exercise/13.2.sum_the_employees.sql
+++ Data Aggregation - Exercise/13.2.sum_the_employees.sql
@@ -2,7 +2,7 @@ SELECT
     COUNT(CASE WHEN department_id = 1 THEN 1 END) AS "Engineering",
 	COUNT(CASE department_id WHEN 2 THEN 1 END) "Tool Design",
 	COUNT(CASE department_id WHEN 3 THEN 1 ELSE NULL END) "Sales",
-	COUNT(CASE department_id WHEN 4 THEN 1 END) "Marketing",
+	COUNT(CASE department_id WHEN 4 THEN 'something' END) "Marketing",
 	COUNT(CASE department_id WHEN 5 THEN 1 END) "Purchasing",
 	COUNT(CASE department_id WHEN 6 THEN 1 END) "Research and Development",
 	COUNT(CASE department_id WHEN 7 THEN 1 END) "Production"
