84a233846c263ce7f652fba5bcbe67a2f96959d0
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-05-28 15:14:54 +0300

    Solution to the task

--- Table Relations - Exercise/03.customs.sql
+++ Table Relations - Exercise/03.customs.sql
@@ -13,4 +13,16 @@ VALUES ('N34FG21B'),
        ('ZE657QP2');
 
 
-select * from passports
+CREATE TABLE
+    people (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(50),
+        salary NUMERIC (10, 2),
+        passport_id INT REFERENCES passports (id)
+);
+
+INSERT INTO
+    people (first_name, salary, passport_id)
+VALUES ('Roberto', 43300.0000, 101),
+       ('Tom', 56100.0000, 102),
+       ('Yana', 60200.0000, 100);
