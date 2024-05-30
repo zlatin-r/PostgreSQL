3ce725b70018d7d1cd4da4fe5d1bfa7040af0e06
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-05-30 14:38:06 +0300

    Solution to the task

--- Table Relations - Exercise/13.set_null.sql
+++ Table Relations - Exercise/13.set_null.sql
@@ -13,7 +13,20 @@ CREATE TABLE contacts (
     CONSTRAINT fk_contacts_customers
     FOREIGN KEY (customer_id)
     REFERENCES customers (id)
-    ON DELETE SET NULL,
-
-
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
+
+INSERT INTO customers (customer_name)
+VALUES ('BlueBird Inc'),
+       ('Dolphin LLC');
+
+INSERT INTO contacts (contact_name, phone, email, customer_id)
+VALUES ('John Doe',	'(408)-111-1234', 'john.doe@bluebird.dev', 1),
+       ('Jane Doe', '(408)-111-1235', 'jane.doe@bluebird.dev',	1),
+       ('David Wright', '(408)-222-1234', 'david.wright@dolphin.dev', 2);
+
+DELETE FROM
+    customers
+WHERE
+    id = 1;
