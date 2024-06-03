ef947eea0069b84d6dea6709177e0eed322725e1
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-03 09:42:48 +0300

    Solution to the task

---
+++ Subqueries and JOINs - Lab/03.employees_projects.sql
@@ -0,0 +1,13 @@
+SELECT
+    e.employee_id,
+    CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+    p.project_id,
+    p.name AS project_name
+FROM
+    employees AS e
+JOIN employees_projects AS ep
+    ON e.employee_id = ep.project_id
+JOIN projects AS p
+    ON p.project_id = ep.project_id
+WHERE
+    ep.project_id = 1
