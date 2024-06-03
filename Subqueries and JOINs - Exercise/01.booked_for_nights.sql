8d3cea2ef3adf8ed2d737fd7e1351acfed65fcec
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-03 10:28:24 +0300

    Solution to the task

--- Subqueries and JOINs - Exercise/01.booked_for_nights.sql
+++ Subqueries and JOINs - Exercise/01.booked_for_nights.sql
@@ -3,7 +3,9 @@ SELECT
     b.booked_for AS nights
 FROM
     apartments AS a
-JOIN bookings AS b
-    ON a.booking_id = b.booking_id
+JOIN
+    bookings AS b
+ON
+    a.booking_id = b.booking_id
 ORDER BY
     a.apartment_id;
