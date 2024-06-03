79edec6952becbe4446c2cec082306e4abee502f
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-03 10:57:17 +0300

    Solution to the task

--- Subqueries and JOINs - Exercise/03.first_10_customers_with_bookings.sql
+++ Subqueries and JOINs - Exercise/03.first_10_customers_with_bookings.sql
@@ -2,7 +2,8 @@ SELECT
     b.booking_id,
     b.starts_at::DATE,
     b.apartment_id,
-    CONCAT(c.first_name, ' ', c.last_name) AS customer_name
+    CONCAT(c.first_name, ' ', c.last_name)
+        AS customer_name
 FROM
     bookings AS b
 RIGHT JOIN
