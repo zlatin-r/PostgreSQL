7b97a4395d577fb4733b98c9bfeed3d04190340e
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-03 09:29:24 +0300

    Solution to the task

--- Subqueries and JOINs - Lab/02.managers.sql
+++ Subqueries and JOINs - Lab/02.managers.sql
@@ -1,12 +1,12 @@
 SELECT
-    employee_id,
+    e.employee_id,
     CONCAT(first_name, ' ', last_name) AS full_name,
     d.department_id,
     d.name AS department_name
 FROM
     employees AS e
-JOIN departments AS d
-    ON e.department_id = d.department_id
+JOIN departments AS d -- or RIGHT JOIN
+    ON d.manager_id = e.employee_id
 ORDER BY
     employee_id
 LIMIT 5;
