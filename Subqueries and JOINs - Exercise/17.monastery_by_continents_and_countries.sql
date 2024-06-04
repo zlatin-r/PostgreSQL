a7215caf521d3ab7dee6d9f4c656828c8b0f10f5
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-04 13:32:03 +0300

    Solution to the task

--- Subqueries and JOINs - Exercise/17.monastery_by_continents_and_countries.sql
+++ Subqueries and JOINs - Exercise/17.monastery_by_continents_and_countries.sql
@@ -5,3 +5,36 @@ SET
 WHERE
     country_name = 'Myanmar';
 
+INSERT INTO
+    monasteries (monastery_name, country_code)
+VALUES
+    ('Hanga Abbey', (SELECT
+                            country_code
+                        FROM
+                            countries
+                        WHERE
+                            country_name = 'Tanzania')),
+    ('Myin-Tin-Daik', 'MY');
+
+SELECT
+    cont.continent_name,
+    coun.country_name,
+    COUNT(coun.country_code) AS monasteries_count
+FROM
+    continents AS cont
+JOIN
+    countries AS coun
+USING
+    (continent_code)
+JOIN
+    monasteries AS mon
+USING
+    (country_code)
+WHERE
+    NOT three_rivers
+GROUP BY
+    coun.country_name,
+    cont.continent_name
+ORDER BY
+    monasteries_count DESC,
+    country_name
