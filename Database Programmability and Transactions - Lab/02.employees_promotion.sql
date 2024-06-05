cdcffbe9ebb4c0f53f7abbed3fb88123e1575e9b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-05 13:13:12 +0300

    Solution to the task

--- Database Programmability and Transactions - Lab/02.employees_promotion.sql
+++ Database Programmability and Transactions - Lab/02.employees_promotion.sql
@@ -4,7 +4,10 @@ $$
     BEGIN
         UPDATE employees AS e
         SET salary = salary * 1.05
-        WHERE e.department_id = (SELECT department_id FROM departments WHERE name = department_name);
+        WHERE e.department_id = (
+            SELECT department_id
+            FROM departments
+            WHERE name = department_name);
     END;
 $$
 LANGUAGE plpgsql;
