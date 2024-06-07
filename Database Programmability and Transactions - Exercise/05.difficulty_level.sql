ad7a0a960c83df5c63ac0988361f00ba26523d38
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-07 08:51:30 +0300

    Solution to the task

--- Database Programmability and Transactions - Exercise/05.difficulty_level.sql
+++ Database Programmability and Transactions - Exercise/05.difficulty_level.sql
@@ -1,1 +1,15 @@
-CREATE OR REPLACE FUNCTION difficulty_level(level VARCHAR(50))
+CREATE OR REPLACE FUNCTION fn_difficulty_level(points INT)
+RETURNS VARCHAR
+AS
+$$
+DECLARE
+    level_is VARCHAR(50);
+BEGIN
+    IF points <= 40 THEN level_is := 'Normal Difficulty';
+    ELSEIF points <= 60 THEN level_is := 'Nightmare Difficulty';
+    ELSE level_is := 'Hell Difficulty';
+    RETURN level_is;
+    END IF;
+END;
+$$
+LANGUAGE plpgsql;
