945e0108a9da533d0d84d0f01fb97a3737942150
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-07 08:28:40 +0300

    Solution to the task

--- Database Programmability and Transactions - Exercise/04.game_over.sql
+++ Database Programmability and Transactions - Exercise/04.game_over.sql
@@ -1,16 +1,21 @@
-CREATE OR REPLACE FUNCTION fn_is_game_over(is_game_over BOOLEAN)
-RETURNS TABLE(name VARCHAR(50), game_type_id INTEGER, is_finished BOOLEAN)
+CREATE OR REPLACE FUNCTION fn_is_game_over(
+    is_game_over BOOLEAN)
+RETURNS TABLE(
+    name VARCHAR(50),
+    game_type_id INTEGER,
+    is_finished BOOLEAN)
 AS
 $$
     BEGIN
+        RETURN QUERY
         SELECT
-            "name",
-            game_type_id,
-            is_finished
+            g.name,
+            g.game_type_id,
+            g.is_finished
         FROM
-            games
+            games AS g
         WHERE
-            is_finished = is_game_over;
+            g.is_finished = is_game_over;
     END;
 $$
 LANGUAGE plpgsql;
