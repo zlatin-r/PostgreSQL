5407e359da6a4d7d2b7cf5e7dc00bf6a6cf1d56c
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-09 11:33:57 +0300

    Solution to the task

---
+++ Exam Preparation/Exam 2/4.11.full_info_address.sql
@@ -0,0 +1,13 @@
+CREATE TABLE search_results
+(
+    id            SERIAL PRIMARY KEY,
+    address_name  VARCHAR(50),
+    full_name     VARCHAR(100),
+    level_of_bill VARCHAR(20),
+    make          VARCHAR(30),
+    condition     CHAR(1),
+    category_name VARCHAR(50)
+);
+
+CREATE OR REPLACE PROCEDURE sp_courses_by_address(address_name VARCHAR(100))
+AS
