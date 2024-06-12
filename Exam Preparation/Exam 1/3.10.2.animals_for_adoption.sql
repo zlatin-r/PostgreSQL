580a107848c53b222ab9cad78187e1e12b0ea3ae
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-12 17:54:20 +0300

    Solution to the task

---
+++ Exam Preparation/Exam 1/3.10.2.animals_for_adoption.sql
@@ -0,0 +1,18 @@
+SELECT
+    a.name AS animal,
+    EXTRACT ('year' FROM a.birthdate) AS birth_year,
+    at.animal_type
+FROM
+    animals AS a
+JOIN
+    animal_types AS at
+ON
+    a.animal_type_id = at.id
+WHERE
+    a.owner_id IS NULL
+    AND
+    at.animal_type <> 'Birds'
+    AND
+    AGE('01/01/2027', a.birthdate) < '5 years'
+ORDER BY
+    a.name;
