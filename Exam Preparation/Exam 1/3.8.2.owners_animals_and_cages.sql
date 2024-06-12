5d00c39c1e7382f5b467403fb2c33d70bdc37943
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-12 17:34:36 +0300

    Solution to the task

---
+++ Exam Preparation/Exam 1/3.8.2.owners_animals_and_cages.sql
@@ -0,0 +1,27 @@
+SELECT
+    CONCAT_WS(' - ', o.name, a.name) AS "owners - animals",
+    o.phone_number,
+    c.id AS cage_id
+FROM
+    animals AS a
+JOIN
+    owners AS o
+ON
+    a.owner_id = o.id
+JOIN
+    animals_cages AS ac
+ON
+    a.id = ac.animal_id
+JOIN
+    cages AS c
+ON
+    ac.cage_id = c.id
+JOIN
+    animal_types AS at
+ON
+    a.animal_type_id = at.id
+WHERE
+    at.animal_type = 'Mammals'
+ORDER BY
+    o.name,
+    a.name DESC;
