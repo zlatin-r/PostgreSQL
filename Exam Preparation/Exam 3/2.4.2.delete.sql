adc15247005fd4d8790283e5e58325b73fd5fb5e
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-13 16:55:21 +0300

    Solution to the task

--- Exam Preparation/Exam 3/2.4.2.delete.sql
+++ Exam Preparation/Exam 3/2.4.2.delete.sql
@@ -1,10 +1,16 @@
 DELETE FROM
     board_games
-WHERE id IN ()
+WHERE publisher_id IN (
+    SELECT id FROM publishers
+              WHERE address_id IN
+    (SELECT id FROM addresses
+               WHERE town LIKE 'L%'));
 
 DELETE FROM
     publishers
-WHERE id IN (SELECT id FROM addresses WHERE town LIKE 'L%');
+WHERE address_id IN (
+    SELECT id FROM addresses
+              WHERE town LIKE 'L%');
 
 DELETE FROM
     addresses
